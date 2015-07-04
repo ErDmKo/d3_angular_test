e23b3b85f24fb7269f97027123851b2f97886482
erdmko <erdmko@gmail.com>
2015-07-04 12:37:56 +0300

    Here i get stop§

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var karma = require('gulp-karma');
+
+var testFiles = [];
+
+gulp.task('test', function() {
+  return gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      throw err;
+    });
+});
+
+gulp.task('default', function() {
+  gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }));
+});
